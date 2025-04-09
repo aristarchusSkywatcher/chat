938fd4e92726628358e0682f950008d541ebb798
Aristarchus <39488312+aristarchusskywatcher@users.noreply.github.com>
2025-04-10 01:00:09 +0300

    Update ModelFactory.php

--- tests/Helpers/factories/ModelFactory.php
+++ tests/Helpers/factories/ModelFactory.php
@@ -1,10 +1,10 @@
 <?php
 
 use Faker\Generator as Faker;
-use Musonza\Chat\Models\Conversation;
-use Musonza\Chat\Tests\Helpers\Models\Bot;
-use Musonza\Chat\Tests\Helpers\Models\Client;
-use Musonza\Chat\Tests\Helpers\Models\User;
+use Aristarchusskywatcher\Chat\Models\Conversation;
+use Aristarchusskywatcher\Chat\Tests\Helpers\Models\Bot;
+use Aristarchusskywatcher\Chat\Tests\Helpers\Models\Client;
+use Aristarchusskywatcher\Chat\Tests\Helpers\Models\User;
 
 /*
 |--------------------------------------------------------------------------
